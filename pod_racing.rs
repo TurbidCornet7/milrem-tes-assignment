5ce022115545647f46a6c0b4234920657bbb91b9
Karl Väärtnõu <vaartnoukarl@gmail.com>
2024-04-14 23:44:32 +0300

    Bronze league

--- pod_racing.rs
+++ pod_racing.rs
@@ -1,15 +1,33 @@
-use std::io;
+use std::{f32::consts::PI, io};
 
 macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
 }
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-fn main() {
+fn distance(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
+    let under = (x1 - x2).pow(2) + (y1 - y2).pow(2);
+    f32::sqrt(under as f32) as i32
+}
 
+fn calculate_thrust(angle: i32, distance: i32) -> i32 {
+    let mut thrust = 100;
+    if angle < -90 || angle > 90 {
+        thrust = 0;
+    };
+    if (distance < 1000) {
+        thrust = 20;
+    };
+    thrust
+}
+
+fn main() {
+    let mut boost_available = true;
     // game loop
     loop {
         let mut input_line = String::new();
@@ -29,12 +47,21 @@ fn main() {
 
         // Write an action using println!("message...");
         // To debug: eprintln!("Debug message...");
-        let thrust = 100 - (next_checkpoint_angle.abs() / 2);
+        eprintln!("Distance to next checkpoint: {}", next_checkpoint_dist);
+
+        let thrust = calculate_thrust(next_checkpoint_angle, next_checkpoint_dist);
+
+        let dist_enemy_to_checkpoint =
+            distance(opponent_x, opponent_y, next_checkpoint_x, next_checkpoint_y);
 
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        println!("{} {} {}", next_checkpoint_x, next_checkpoint_y, thrust);
+        if next_checkpoint_dist > dist_enemy_to_checkpoint && boost_available && thrust == 100 {
+            println!("{} {} {}", next_checkpoint_x, next_checkpoint_y, "BOOST");
+            boost_available = false;
+        } else {
+            println!("{} {} {}", next_checkpoint_x, next_checkpoint_y, thrust);
+        }
     }
 }
-
